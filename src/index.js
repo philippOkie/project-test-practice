ed4bc2ed43d8f76450de19b713eb9f97f454416a
philippOkie <kriukphilipp@gmail.com>
2023-09-27 13:23:02 +0300

    add capitalize function and tests for it

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-export function capitalize() {
-  return "Hi mom";
+export function capitalize(str) {
+  let firstChar = str.slice(0, 1);
+  if (isCharLetter(firstChar)) {
+    firstChar = firstChar.toUpperCase();
+    let newStr = str.substring(1);
+    return firstChar + newStr;
+  } else {
+    return "invalid input";
+  }
+}
+
+function isCharLetter(firstChar) {
+  return firstChar.toLowerCase() !== firstChar.toUpperCase();
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,17 @@
 import { capitalize } from "./index.js";
 
 test("Capitalizing the first letter", () => {
-  expect(capitalize("hi mom").toBe("Hi mom"));
+  expect(capitalize("hi mom")).toBe("Hi mom");
+});
+
+test("First letter stays the same if capitalized", () => {
+  expect(capitalize("Hi mom")).toBe("Hi mom");
+});
+
+test("The func doesn't do anything without an appropriate input", () => {
+  expect(capitalize("12334354#$%")).toBe("invalid input");
+});
+
+test("Capitalizing the first letter", () => {
+  expect(capitalize("test")).toBe("Test");
 });
