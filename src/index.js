6e0dd05db673a8660ffe62bb776e1ec67d7c6e6e
philippOkie <kriukphilipp@gmail.com>
2023-09-28 12:17:42 +0300

    add analyze array func and its tests

--- src/index.js
+++ src/index.js
@@ -1,67 +1,86 @@
-export function capitalize(str) {
-  let firstChar = str.slice(0, 1);
-  if (isCharLetter(firstChar)) {
-    firstChar = firstChar.toUpperCase();
-    let newStr = str.substring(1);
-    return firstChar + newStr;
-  } else {
-    return "invalid input";
-  }
-}
+// export function capitalize(str) {
+//   let firstChar = str.slice(0, 1);
+//   if (isCharLetter(firstChar)) {
+//     firstChar = firstChar.toUpperCase();
+//     let newStr = str.substring(1);
+//     return firstChar + newStr;
+//   } else {
+//     return "invalid input";
+//   }
+// }
 
-function isCharLetter(firstChar) {
-  return firstChar.toLowerCase() !== firstChar.toUpperCase();
-}
+// function isCharLetter(firstChar) {
+//   return firstChar.toLowerCase() !== firstChar.toUpperCase();
+// }
 
-export function reverse(x) {
-  let str = x.toString();
-  let xStringReversed = "";
-  let i = str.length - 1;
-  while (i >= 0) {
-    xStringReversed += str.substring(i, i + 1);
-    i--;
-  }
-  return xStringReversed;
-}
+// export function reverse(x) {
+//   let str = x.toString();
+//   let xStringReversed = "";
+//   let i = str.length - 1;
+//   while (i >= 0) {
+//     xStringReversed += str.substring(i, i + 1);
+//     i--;
+//   }
+//   return xStringReversed;
+// }
 
-export default class Calculator {
-  add(a, b) {
-    let result = a + b;
-    return result;
-  }
+// export default class Calculator {
+//   add(a, b) {
+//     let result = a + b;
+//     return result;
+//   }
 
-  subtract(a, b) {
-    let result = a - b;
-    return result;
-  }
+//   subtract(a, b) {
+//     let result = a - b;
+//     return result;
+//   }
 
-  multiply(a, b) {
-    let result = a * b;
-    return result;
-  }
+//   multiply(a, b) {
+//     let result = a * b;
+//     return result;
+//   }
 
-  divide(a, b) {
-    let result = a / b;
-    return result;
-  }
-}
+//   divide(a, b) {
+//     let result = a / b;
+//     return result;
+//   }
+// }
 
-export function caesarCipher(strPlain, key) {
-  debugger;
+// export function caesarCipher(strPlain, key) {
+//   let result = [];
+//   for (let i = 0; i < strPlain.length; i++) {
+//     let code = strPlain.charCodeAt(i);
+//     let toCheck = String.fromCharCode(code);
+//     if (isCharLetter(toCheck)) {
+//       code = strPlain.charCodeAt(i) + key;
+//       while (code > 122) {
+//         code = code - 122 + 96;
+//       }
+//       result.push(String.fromCharCode(code));
+//       console.log(result);
+//     } else {
+//       result.push(String.fromCharCode(code));
+//     }
+//   }
+//   return result.join("");
+// }
+
+export function analyzeArray(arr) {
   let result = [];
-  for (let i = 0; i < strPlain.length; i++) {
-    let code = strPlain.charCodeAt(i);
-    let toCheck = String.fromCharCode(code);
-    if (isCharLetter(toCheck)) {
-      code = strPlain.charCodeAt(i) + key;
-      while (code > 122) {
-        code = code - 122 + 96;
-      }
-      result.push(String.fromCharCode(code));
-      console.log(result);
-    } else {
-      result.push(String.fromCharCode(code));
-    }
+  let counter = 0;
+  let len = 0;
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    counter++;
+    sum += arr[i];
+    len = counter;
   }
-  return result.join("");
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  let avrg = sum / counter;
+  result.push(avrg);
+  result.push(min);
+  result.push(max);
+  result.push(len);
+  return result;
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,63 +1,69 @@
-import Calculator, { capitalize, reverse, caesarCipher } from "./index.js";
-// capitalize tests
-test("Capitalizing the first letter", () => {
-  expect(capitalize("hi mom")).toBe("Hi mom");
-});
+import { analyzeArray } from "./index.js";
+// // capitalize tests
+// test("Capitalizing the first letter", () => {
+//   expect(capitalize("hi mom")).toBe("Hi mom");
+// });
 
-test("First letter stays the same if capitalized", () => {
-  expect(capitalize("Hi mom")).toBe("Hi mom");
-});
+// test("First letter stays the same if capitalized", () => {
+//   expect(capitalize("Hi mom")).toBe("Hi mom");
+// });
 
-test("The func doesn't do anything without an appropriate input", () => {
-  expect(capitalize("12334354#$%")).toBe("invalid input");
-});
+// test("The func doesn't do anything without an appropriate input", () => {
+//   expect(capitalize("12334354#$%")).toBe("invalid input");
+// });
 
-test("Capitalizing the first letter", () => {
-  expect(capitalize("test")).toBe("Test");
-});
-// reverse a str tests
-test("reverse a sentence", () => {
-  expect(reverse("hi mom")).toBe("mom ih");
-});
+// test("Capitalizing the first letter", () => {
+//   expect(capitalize("test")).toBe("Test");
+// });
+// // reverse a str tests
+// test("reverse a sentence", () => {
+//   expect(reverse("hi mom")).toBe("mom ih");
+// });
 
-test("reverse a num", () => {
-  expect(reverse("12345")).toBe("54321");
-});
+// test("reverse a num", () => {
+//   expect(reverse("12345")).toBe("54321");
+// });
 
-test("reverse a word", () => {
-  expect(reverse("test")).toBe("tset");
-});
-// calculator tests
-test("subtract", () => {
-  const calculator = new Calculator();
-  expect(calculator.subtract(13, 4)).toBe(9);
-});
+// test("reverse a word", () => {
+//   expect(reverse("test")).toBe("tset");
+// });
+// // calculator tests
+// test("subtract", () => {
+//   const calculator = new Calculator();
+//   expect(calculator.subtract(13, 4)).toBe(9);
+// });
 
-test("add", () => {
-  const calculator = new Calculator();
-  expect(calculator.add(2, 2)).toBe(4);
-});
+// test("add", () => {
+//   const calculator = new Calculator();
+//   expect(calculator.add(2, 2)).toBe(4);
+// });
 
-test("multiply", () => {
-  const calculator = new Calculator();
-  expect(calculator.multiply(5, 5)).toBe(25);
-});
+// test("multiply", () => {
+//   const calculator = new Calculator();
+//   expect(calculator.multiply(5, 5)).toBe(25);
+// });
 
-test("divide", () => {
-  const calculator = new Calculator();
-  expect(calculator.divide(9, 3)).toBe(3);
-});
-// caesar cipher tests
-test("Caesar cipher, test 1", () => {
-  expect(caesarCipher("defend the east wall of the castle", 1)).toBe(
-    "efgfoe uif fbtu xbmm pg uif dbtumf"
-  );
-});
+// test("divide", () => {
+//   const calculator = new Calculator();
+//   expect(calculator.divide(9, 3)).toBe(3);
+// });
+// // caesar cipher tests
+// test("Caesar cipher, test 1", () => {
+//   expect(caesarCipher("defend the east wall of the castle", 1)).toBe(
+//     "efgfoe uif fbtu xbmm pg uif dbtumf"
+//   );
+// });
 
-test("Caesar cipher, test 2", () => {
-  expect(caesarCipher("Hi mom", 3)).toBe("Kl prp");
-});
+// test("Caesar cipher, test 2", () => {
+//   expect(caesarCipher("Hi mom", 3)).toBe("Kl prp");
+// });
 
-test("Caesar cipher, test 3", () => {
-  expect(caesarCipher("!hi mom666", 7)).toBe("!op tvt666");
+// test("Caesar cipher, test 3", () => {
+//   expect(caesarCipher("!hi mom666", 7)).toBe("!op tvt666");
+// });
+// analyze array tests
+
+const result = analyzeArray([1, 8, 3, 4, 2, 6]);
+test("analyze array", () => {
+  expect(result).toStrictEqual([4, 1, 8, 6]);
 });
